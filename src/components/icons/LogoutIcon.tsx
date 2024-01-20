44f35ae3256d4cdc01ba5b304ccf378c2ca26d0e
Jhon Hernandez <jhobahego16@hotmail.com>
2024-01-19 21:27:25 -0500

    Add Iconos LoginIcon y LogoutIcon

---
+++ src/components/icons/LogoutIcon.tsx
@@ -0,0 +1,18 @@
+export const LogoutIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+        <path d="M6 21v-2a4 4 0 0 1 4 -4h4c.348 0 .686 .045 1.009 .128" />
+        <path d="M16 19h6" />
+    </svg>
+  )
+}
