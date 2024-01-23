800656c58e91f78dfddd36956071cf1c97e288c9
Jhon Hernandez <jhobahego16@hotmail.com>
2024-01-22 23:46:36 -0500

    Update API endpoints en el servicio de tareas

--- src/services/task.ts
+++ src/services/task.ts
@@ -3,21 +3,21 @@ import type { TodoTitle, Todo, TodoId, ApiResponse } from '../types'
 import { type AxiosResponse } from 'axios'
 
 export async function getTodos (): Promise<AxiosResponse<Todo[]>> {
-  return await axiosInstance.get('')
+  return await axiosInstance.get('/api/tareas')
 }
 
 export async function createTodo ({ title }: TodoTitle): Promise<AxiosResponse<ApiResponse>> {
-  return await axiosInstance.post('', { title }, { headers: { 'Content-Type': 'application/json' } })
+  return await axiosInstance.post('/api/tareas', { title }, { headers: { 'Content-Type': 'application/json' } })
 }
 
 export async function deleteTodo ({ id }: TodoId) {
-  return await axiosInstance.delete(`/eliminar/${id}`)
+  return await axiosInstance.delete(`/api/tareas/eliminar/${id}`)
 }
 
 export async function deleteAllCompleted (): Promise<void> {
-  await axiosInstance.delete('/completadas')
+  await axiosInstance.delete('/api/tareas/completadas')
 }
 
 export async function completeTodo ({ id }: TodoId): Promise<AxiosResponse<ApiResponse>> {
-  return await axiosInstance.patch(`/completar/${id}`)
+  return await axiosInstance.patch(`/api/tareas/completar/${id}`)
 }
