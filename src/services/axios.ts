2bf6857e471ee64ab70b0c8d4ced79b13477f0d0
Jhon Hernandez <jhobahego16@hotmail.com>
2024-01-23 23:27:01 -0500

    Update Content-Type header en axios service

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -5,7 +5,7 @@ const BASE_URL = import.meta.env.VITE_BASE_URL
 const axiosIntance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
-    'Content-Type': 'multipart/form-data',
+    'Content-Type': 'application/json',
     Accept: 'application/json'
   }
 })
